cfa5fca1ff7de05faeeacfe285a97bd5f14e53b0
Glauco Moraes <glaucomsm@gmail.com>
2016-09-01 15:11:48 -0300

    Initialization

---
+++ cellRegistration/cellRegistration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cellRegistration
+//
+//  Created by Glauco Moraes on 01/09/16.
+//  Copyright © 2016 MoBeer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
